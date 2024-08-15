a26f268c02274c2a70464e80f10ee4d25f22483c
Soham Patel <sohampat@umich.edu>
2024-08-15 17:31:13 -0400

    Added options to view categories and overview in piechart form

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 #each individual category is give by this it will have a catalog, a name, and a total cost
 class Category:
     def __init__(self, name):
         self.name = name
         self.catalog = {}
         self.total = 0   
+    def addition(self):
+        self.total = sum(self.catalog.values())
 def is_float(string):
     if string.replace(".","").isnumeric():
         return True
@@ -13,19 +16,100 @@ def is_float(string):
         return False
 #This function will present the data in a nice manner preferably a graph or chart of sorts
 def showData(amountDict, depositDict):
-    expenseLabel, expenseNum = zip(*amountDict.items())
-    depositLabel, depositNum = zip(*depositDict.items())
-    fig, (ax1, ax2) = plt.subplots(1,2,figsize=(10,10))
-    ax1.pie(expenseNum, labels = expenseLabel)
-    ax1.set_title('Expenses')
-    ax2.pie(depositNum, labels = depositLabel)
-    ax2.set_title('Income')
-    plt.show()
+    while True:
+        print("\nWould you like to see a specific Category or Overiew?\n1.Category\n2.Overview\n")
+        represenation = input("\nType 1 or 2\n")
+        if not(int(represenation) == 1 or int(represenation) == 2):
+            print("Please Enter 1 or 2")
+            continue
+        break
+    if int(represenation) == 2:
+        expenseNum = []
+        expenseLabel = []
+        depositNum = []
+        depositLabel = []
+        for i in amountDict:
+            i.addition()
+            expenseNum.append(i.total)
+            expenseLabel.append(i.name)
+        for j in depositDict:
+            j.addition()
+            depositNum.append(j.total)
+            depositLabel.append(j.name)
 
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
+        totalExpense = sum(expenseNum)
+        totalDeposit = sum(depositNum)
+        for i in range(len(depositNum)):
+            depositLabel[i] = f"{depositLabel[i]}: {depositNum[i]/totalDeposit *100:.1f}%"
+        for j in range(len(expenseNum)):
+            expenseLabel[j] = f"{expenseLabel[j]}: {expenseNum[j]/totalExpense *100:.1f}%"
+        # Plotting the pie chart for expenses
+        ax1.pie(expenseNum, startangle=140)
+        ax1.set_title('Expenses: ${totalExpense}')
+        ax1.legend(expenseLabel, loc="best")
+
+        # Plotting the pie chart for deposits
+        ax2.pie(depositNum, startangle=140)
+        ax2.set_title('Income: ${totalDeposit}')
+        ax2.legend(depositLabel, title="Income", loc="best")
+        plt.tight_layout()
+        plt.show()
+    elif int(represenation) == 1:
+        label = []
+        numbers = []
+        index = 0
+        total = 0
+        while True:
+            print("\nWould you like to see:\n")
+            section = input("\n1.Deposit\n2.Expense\nType the corresponding number 1 or 2\n")
+            if not int(section) == 1 and not int(section) == 2:
+                print("Please enter a number")
+                continue
+            break
+        if int(section) == 2:
+            currentSec = amountDict
+            print("\nHere are the Expenses Categories\n")
+        elif int(section) == 1:
+            currentSec = depositDict
+            print("\nHere are the Deposit Categories\n")
+        while True:
+            for index, item in enumerate(currentSec):
+                print(f'{index}. {item.name}')
+            category = input("\nPlease enter the number corresponding to the category\n")
+            if(not is_float(category) or int(category) >= len(amountDict)):
+                print("Please enter a number")
+                continue
+            currentCatalog = currentSec[int(category)].catalog
+            currentSec[int(category)].addition()
+            total = currentSec[int(category)].total
+            index = int(category)
+            if total == 0:
+                print("Sorry this category has no data, Please select another one")
+                continue
+            label = list(currentCatalog.keys())
+            numbers = list(currentCatalog.values())
+            break
+        for i in range(len(numbers)):
+            label[i] = f"{label[i]}: {numbers[i]/total *100:.1f}%"
+        # Plotting the pie chart for expenses
+        fig, (ax1) = plt.subplots(figsize=(14, 7))
+        ax1.pie(numbers, startangle=140)
+        ax1.set_title(f'{currentSec[index].name}: ${total}')
+        ax1.legend(label, loc="best")
+
+        plt.tight_layout()
+        plt.show()
+            
+def saveData(amountDict, depositDict):
+    with open('day_one.csv') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(amountDict)
+        writer.writerows(depositDict)
 #This function will collect the expense
 def withdraw(amountDict):
     while True:
-        print("\nDeposit Categories:\n")
+        print("\nExpenses Categories:\n")
         for index, item in enumerate(amountDict):
             print(f'{index}. {item.name}')
         category = input("\nPlease enter the number corresponding to the category\n")
@@ -35,11 +119,11 @@ def withdraw(amountDict):
         amount = input("How much did it cost? \nAmount: ")
         reason = input("What was it? \nWhat: ")
         if(reason.isalpha() and is_float(amount)):
-            if(amountDict.get(str(reason.lower())) == None):
-                amountDict[str(reason.lower())] = float(amount)
+            if(amountDict[int(category)].catalog.get(str(reason.lower())) == None):
+                amountDict[int(category)].catalog[str(reason.lower())] = float(amount)
             else:
-                newAmount = amountDict.get(str(reason.lower())) + float(amount)
-                amountDict[str(reason.lower())] = float(newAmount)
+                newAmount = amountDict[int(category)].catalog.get(str(reason.lower())) + float(amount)
+                amountDict[int(category)].catalog[str(reason.lower())] = float(newAmount)
             break
         else:
             print("Please make sure the What is all alphabets and Amount is only number")
@@ -107,4 +191,5 @@ def main():
         if(choice == 'Y'):
             break
     showData(expenseDict, depositDict)
+    #saveData(expenseDict, depositDict)
 main()
