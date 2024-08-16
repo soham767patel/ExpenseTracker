6429a1423b3e7ec1c0385eadfd74a744f7632e74
Soham Patel <sohampat@umich.edu>
2024-08-16 14:12:22 -0400

    Saving is working somewhat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import os.path
 #each individual category is give by this it will have a catalog, a name, and a total cost
 class Category:
     def __init__(self, name):
@@ -11,6 +12,11 @@ class Category:
         self.total = sum(self.catalog.values())
 def is_float(string):
     if string.replace(".","").isnumeric():
+        return True
+    else:
+        return False
+def is_alphabet(string):
+    if string.replace(" ","").isalpha():
         return True
     else:
         return False
@@ -99,9 +105,8 @@ def showData(amountDict, depositDict):
         ax1.legend(label, loc="best")
 
         plt.tight_layout()
-        plt.show()
-            
-def saveData(amountDict, depositDict):
+        plt.show()      
+def saveData(amountDict, depositDict, currDate):
     print("\nSaving Data . . . ")
     
     # Prepare labels
@@ -123,7 +128,7 @@ def saveData(amountDict, depositDict):
         headersDeposit.append([f'{jtem.name}: Key'])
         headersDeposit.append([f'{jtem.name}: Num'])
     # Write labels to CSV file
-    with open('day_one_expense.csv', 'w', newline='') as csvfile:
+    with open(f'{currDate}_expense.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(headersAmount)
         for i in range(maxLengthExp):
@@ -136,7 +141,7 @@ def saveData(amountDict, depositDict):
                     row.append("NaNa")
                     row.append(0)
             writer.writerow(row)
-    with open('day_one_deposit.csv', 'w', newline='') as csvfile:
+    with open(f'{currDate}_deposit.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(headersDeposit)
         for i in range(maxLengthDep):
@@ -150,6 +155,47 @@ def saveData(amountDict, depositDict):
                     row.append(0)
             writer.writerow(row)
 #This function will collect the expense
+def inputMode(date, expenseDict, depositDict):
+    currentDirectory = os.getcwd()
+    currentPathExpense = currentDirectory + f'/{date}_expense.csv'
+    currentPathDeposit = currentDirectory + f'/{date}_deposit.csv'
+    if os.path.exists(currentPathDeposit):
+        print(f'{date} deposit file exists . . . \n')
+        file = f'{date}_deposit.csv'
+        with open(file, 'r') as csvfile:
+            next(csvfile)
+            reader = csv.reader(csvfile)
+            for row in reader:
+                for i in range(0, 12, 2):
+                    if(row[i] == "NaNa"):
+                        pass
+                    else:
+                        if depositDict[int(i/2)].catalog.get(row[i]) == None :
+                            depositDict[int(i/2)].catalog[row[i]] = float(row[i+1])
+                        else:
+                            newAmount = depositDict[i/2].catalog.get(row[i]) + float(row[i+1])
+                            depositDict[int(i/2)].catalog[row[i]] = float(newAmount) 
+    else:
+        print(f'{date} deposit file does not exist . . . not uploading . . . \n')
+    if os.path.exists(currentPathExpense):
+        print(f'{date} expense file exists . . .\n')
+        file = f'{date}_expense.csv'
+        with open(file, 'r') as csvfile:
+            next(csvfile)
+            reader = csv.reader(csvfile)
+            for row in reader:
+                for i in range(0, 20, 2):
+                    if(row[i] == "NaNa"):
+                        pass
+                    else:
+                        if expenseDict[int(i/2)].catalog.get(row[i]) == None :
+                            expenseDict[int(i/2)].catalog[row[i]] = float(row[i+1])
+                        else:
+                            newAmount = expenseDict[int(i/2)].catalog.get(row[i]) + float(row[i+1])
+                            expenseDict[int(i/2)].catalog[row[i]] = float(newAmount) 
+    else:
+        print(f'{date} expense file does not exist . . . not uploading . . . \n')
+    return expenseDict, depositDict
 def withdraw(amountDict):
     while True:
         print("\nExpenses Categories:\n")
@@ -161,7 +207,7 @@ def withdraw(amountDict):
             continue
         amount = input("How much did it cost? \nAmount: ")
         reason = input("What was it? \nWhat: ")
-        if(reason.isalpha() and is_float(amount)):
+        if(is_alphabet(reason) and is_float(amount)):
             if(amountDict[int(category)].catalog.get(str(reason.lower())) == None):
                 amountDict[int(category)].catalog[str(reason.lower())] = float(amount)
             else:
@@ -178,19 +224,21 @@ def deposit(depositDict):
         print("\nDeposit Categories:\n")
         for index, item in enumerate(depositDict):
             print(f'{index}. {item.name}')
-        category = input("\nPlease enter the number corresponding to the category\n")
+        category = input("\nPlease enter the number corresponding to the category OR (Type L) to Exit Depositing\n")
+        if category.lower() == 'l':
+            "\nAll done with Income . . .\n"
+            break
         if(not is_float(category) or int(category) >= len(depositDict)):
             print("Please enter a number")
             continue
         amount = input("\nHow much did you add to the account?\nAmount: ")
         reason = input("\nWho is providing this deposit?\nSource: ")
-        if(reason.isalpha() and is_float(amount)):
+        if(is_alphabet(reason) and is_float(amount)):
             if(depositDict[int(category)].catalog.get(str(reason.lower())) == None):
                 depositDict[int(category)].catalog[str(reason.lower())] = float(amount)
             else:
                 newAmount = depositDict[int(category)].catalog.get(str(reason.lower())) + float(amount)
                 depositDict[int(category)].catalog[str(reason.lower())] = float(newAmount)
-            break
         else:
             print("Please make sure the Source is all alphabets and Amount is only a number")
             continue
@@ -200,10 +248,20 @@ def main():
     depositCategory = ["Employment Income", "Self-Employment Income","Investment Income","Rental Income","Government Assistance","Other Income"]
     expenseDict = []
     depositDict = []
+    date = ''
+    while True: 
+        print("\nWhat is today's date?\n")
+        date = input("In YYYYMMDD Format: ")
+        if(len(date) == 8 and date.isnumeric()):
+            break
+        else:
+            "Please enter only numbers in YYYYMMDD Format!"
+            continue
     for i in expensesCategory:
         expenseDict.append(Category(i))
     for j in depositCategory:
         depositDict.append(Category(j))
+    expenseDict, depositDict = inputMode(date, expenseDict, depositDict)
     while True:
         #loop for making sure that we get correct response
         while True:
@@ -233,6 +291,6 @@ def main():
                 print("Please only type Y or N")
         if(choice == 'Y'):
             break
-    saveData(expenseDict, depositDict)
+    saveData(expenseDict, depositDict, date)
     showData(expenseDict, depositDict)
 main()
