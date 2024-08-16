49ae661c404f19718991762c937a4c2bea06a5ed
Soham Patel <sohampat@umich.edu>
2024-08-16 15:13:56 -0400

    Adding some tkinter testing

--- main.py
+++ main.py
@@ -298,8 +298,11 @@ def test():
     root = Tk()
     #Creating label widgit
     myLabel = Label(root, text = "Hello World!")
+    myLabel2 = Label(root, text= "My Name Is John Elder")
+    myLabel3 = Label(root, text= "")
     #Shoving it onto the screen
-    myLabel.pack()
-
+    myLabel.grid(row=0, column = 0)
+    myLabel2.grid(row=1, column = 5)
+    myLabel3.grid(row=1, column=1)
     root.mainloop()
 test()
